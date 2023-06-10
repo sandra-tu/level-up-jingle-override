8ecf2c519d958237f9a4576428a32720b5fa8af3
Sand Rat <sandrat@Sandras-MacBook-Pro.local>
2023-06-10 18:47:58 +0100

    [CHORE] name change and config

--- src/test/java/com/levelupjingleoverride/LevelUpJingleOverridePluginTest.java
+++ src/test/java/com/levelupjingleoverride/LevelUpJingleOverridePluginTest.java
@@ -1,13 +1,14 @@
-package com.example;
+package com.levelupjingleoverride;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class LevelUpJingleOverridePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		System.out.println("hi");
+		ExternalPluginManager.loadBuiltin(LevelUpJingleOverridePlugin.class);
 		RuneLite.main(args);
 	}
 }
